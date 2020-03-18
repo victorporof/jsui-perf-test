1ca25b0037c842edbc88d036a2898f3ab878ec29
Victor <victor.porof@gmail.com>
2020-03-18 08:01:24 +0100

    Fix import typo
    Signed-off-by: Victor <victor.porof@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import "./benchmark";
 
 import "./css/harness.css";
 import "./css/content.css";
+
 import "./css/layout-flex.css";
-// import "./layout-block.css";
+// import "./css/layout-block.css";
 
 import "./util/fps";
 import "./util/fps.css";
